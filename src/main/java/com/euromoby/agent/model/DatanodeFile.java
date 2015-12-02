a534f44658c8a537f21e15699d0e1865ea9a3325
Lameaux <sergii.sizov@gmail.com>
2015-12-02 17:15:18 +0100

    DatanodeFile

---
+++ src/main/java/com/euromoby/agent/model/DatanodeFile.java
@@ -0,0 +1,24 @@
+package com.euromoby.agent.model;
+
+public class DatanodeFile {
+
+	private String id;
+	private long size;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public long getSize() {
+		return size;
+	}
+
+	public void setSize(long size) {
+		this.size = size;
+	}
+
+}
